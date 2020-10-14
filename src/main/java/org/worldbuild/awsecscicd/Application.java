6ed8308a63a61db2615792741f3b04355dd02448
Your Name <you@example.com>
2020-10-14 14:31:05 +0000

    log added

--- src/main/java/org/worldbuild/awsecscicd/Application.java
+++ src/main/java/org/worldbuild/awsecscicd/Application.java
@@ -2,19 +2,26 @@ package org.worldbuild.awsecscicd;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.worldbuild.core.config.CoreConfiguration;
 
+import java.util.Date;
+
 @Data
 @Slf4j
 @Import(CoreConfiguration.class)
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Current Date =====> "+new Date());
+	}
 }
